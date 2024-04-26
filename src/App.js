d4ad9bd0715bbf5606b7e7e865e7376fb0561f32
Mutyala Mahitha Mallarapu <mutyalamahitha1998@gmail.com>
2024-04-26 13:32:17 +0530

    Added Login Pge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Components/pages/loginPage/LoginPage'
 
 function App() {
-  return <Login />
+   return <Login />
+  // return <Router>
+  //    <div>
+  //       {/* Create links to navigate */}
+  //       <nav>
+  //         <ul>
+  //           <li>
+  //             <Link to="/">Home</Link>
+  //           </li>
+  //           <li>
+  //             <Link to="/login">Login</Link>
+  //           </li>
+  //           <li>
+  //             <Link to="/contact">Contact</Link>
+  //           </li>
+  //         </ul>
+  //       </nav>
+  //       </div>
+  // <Route path="/login" exact component={Login} />
+  // </Router>
 }
 
 export default App;
--- src/Components/pages/loginPage/LoginPage.jsx
+++ src/Components/pages/loginPage/LoginPage.jsx
@@ -1,26 +1,16 @@
-import backgroundImage from "../../../assets/himalaya_mountains_4k.jpg";
+import ERAButton from "../../atoms/ERAButton/ERAButton";
 import  ERAInput  from "../../atoms/ERAInput/ERAInput";
+import './LoginPage.css'
 
 const Login = () => {
   return (
-    <div
-      style={{
-        background: `url(${backgroundImage})`,
-        height: "100vh",
-        backgroundSize: "cover",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-        }}
-      >
-        
-<br />
+    <div className="backgroundStyling">
+      <div className="loginStyle" >
+        <p className="haveAnAcc"> Have an account ? </p>
         <ERAInput name="User Name" type ="Text"/>
         <ERAInput name="Password" type ="Password"/>
+        <ERAButton Name="Login"/>
+        <p className="signUp"> New to EduReact App, <href  className="signUpLink"> Signup </href> </p> 
       </div>
     </div>
   );
